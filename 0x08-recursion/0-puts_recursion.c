ac51b984d495b8e32b309ec8c29782002a18c15e
Abdullahi Abdulsalam <Owunaabdulsalam@gmail.com>
2023-06-07 06:03:06 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * _puts_recursion - function luke puts();
  * @s: input
  * Return: Always 0 (Success)
  */
-
 void _puts_recursion(char *s);
 {
 	if (*s)
@@ -13,6 +11,7 @@ void _puts_recursion(char *s);
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	
 	else
 		_putchar('\n');
 }

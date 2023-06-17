d14abe64d097fb293436bcb9d66516a625bdd4b9
Abdullahi Abdulsalam <Owunaabdulsalam@gmail.com>
2023-06-17 05:11:21 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,16 @@
 /**
  * _puts_recursion - function like puts();
  * @s: input
- * Return: 0
+ * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
-	if (s)
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+
 	else
-	{
 		_putchar('\n');
-	}
 }
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,2 @@
+Project
 0x08. C - Recursion
-Recursion is a method where the solution is a problem that depends on solutions to smaller instances of the same problem.
